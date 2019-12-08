ebbecbf49b9f408b8b4b198dc9674032064a5446
Igor Aragão <roginaldosemog@gmail.com>
2019-12-08 17:14:40 -0300

    Adiciona função de posicionamento das imagens

--- src/images.py
+++ src/images.py
@@ -28,6 +28,23 @@ def resizeImage(image, width, height):
     resized_image = image.resize((width, height))
     return resized_image
 
+# center image
+def imageToImage(position, image, background):
+    img_w, img_h = image.size
+    bg_w, bg_h = background.size
+    offset = getPositionFeed(position, bg_w, bg_h, img_w, img_h)
+    background.paste(image, offset, image)
+    return background
+
+# Adiciona texto à imagem
+def textToImage(position, image, text, size, color, font):
+    img_w, img_h = image.size
+    font = ImageFont.truetype('src/fonts/Montserrat/{}.ttf'.format(font), size=size)
+    w,h = font.getsize(text)
+    draw = ImageDraw.Draw(image)
+    draw.text(getPositionFeed(position, img_w, img_h, w, h), text, font=font, fill=color)
+    return image
+
 # Abre a imagem na aplicação padrão do sistema
 def showImage(image):
     image.show()
@@ -36,30 +53,37 @@ def showImage(image):
 def saveImage(image, name, extension):
     image.save('img/' + name, extension)
 
-# center image
-def imageToImage(image, background):
-    img_w, img_h = image.size
-    bg_w, bg_h = background.size
-    offset = ((bg_w - img_w) // 2, (bg_h - img_h) // 2)
-    background.paste(image, offset, image)
-    return background
-
-# Adiciona texto à imagem
-def textToImage(image, text, size, color):
-    img_w, img_h = image.size
-    font = ImageFont.truetype('src/fonts/Montserrat/Montserrat-Bold.ttf', size=size)
-    w,h = font.getsize(text)
-    draw = ImageDraw.Draw(image)
-    draw.text(((img_w-w)/2, (img_h-h)/2), text, font=font, fill=color)
-    return image
+# Funções que retornam posicionamento de imagem
+def getPositionFeed(mode, img_w, img_h, width, height):
+    if (mode == 'top-left'):
+        return (44, 44)
+    elif (mode == 'top-center'):
+        return (((img_w-width)//2), 44)
+    elif (mode == 'top-right'):
+        return (((img_w-width)-44), 44)
+    elif (mode == 'center-left'):
+        return (44, (img_h-height)//2)
+    elif (mode == 'center'):
+        return (((img_w-width)//2), (img_h-height)//2)
+    elif (mode == 'center-right'):
+        return (((img_w-width)-44), (img_h-height)//2)
+    elif (mode == 'bottom-left'):
+        return (44, (img_h-height)-44)
+    elif (mode == 'bottom-center'):
+        return (((img_w-width)//2), (img_h-height)-44)
+    elif (mode == 'bottom-right'):
+        return (((img_w-width)-44), (img_h-height)-44)
+    else:
+        return ((img_w-width)//2, (img_h-height)//2)
 
 # Código teste
 if __name__ == "__main__":
-    feed = createImage(WIDTH, FEED_HEIGHT, '#406040')
-    # feed = loadImage('img/background.jpg')
+    # feed = createImage(WIDTH, FEED_HEIGHT, '#406040')
+    feed = loadImage('img/background.jpg')
     logo = loadImage('img/logo.png')
-    feed = imageToImage(logo, feed)
-    feed = textToImage(feed, 'UMIC MOOVE', 120, 'white')
+    logo = resizeImage(logo, 96, 96) # TODO: aspect-ratio && transparency
+    feed = imageToImage('bottom-center', logo, feed)
+    feed = textToImage('center', feed, 'UMIC MOOVE', 120, 'white', 'Montserrat-Bold')
     saveImage(feed, 'feed.png', 'JPEG')
     feed = resizeImage(feed, 512, 512)
     showImage(feed)
